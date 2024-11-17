5f8d5245edbf744f9fb8af589fd8c9d829c1ba93
garussek <elena.gooss@gmail.com>
2024-11-17 12:58:10 +0300

    Add files via upload

---
+++ modul_1_3.py
@@ -0,0 +1,28 @@
+name = "Elena"
+print("name:" , name)
+age = 63
+print("age:" , age)
+new_age = 64
+print("new_age:" , (age + 1))
+is_student = "True"
+print("is_student:" , is_student)
+
+
+
+total_tasks = 12
+print("total_tasks:", total_tasks)
+hours_spent = 1.5
+print("hors_spent:", hours_spent)
+course = "Python"
+print("course:", course)
+average_time = 0.125
+print("average_time:", (hours_spent / total_tasks))
+print("course:",course,"total_tasks:",total_tasks,"hors_spent:",hours_spent,"average_time:",(hours_spent / total_tasks))
+
+
+example = "Топинамбур"
+print(example [0])
+print(example [-1])
+print(example [5 :])
+print(example [: : -1])
+print(example [1 : 10 :2])
